765fa146f27e84acb976ffbb0d1d1ef0fbf1124e
Henilgajera <henilgajera123@gmail.com>
2024-08-31 23:07:06 +0530

    Define Library and Book Class

---
+++ LibraryManagement.py
@@ -0,0 +1,11 @@
+class Book:
+    def __init__(self, isbn, title, author, publication_year):
+        self.isbn = isbn
+        self.title = title
+        self.author = author
+        self.publication_year = publication_year
+        self.is_available = True
+
+class Library:
+    def __init__(self):
+        self.books = {}
--- TestLibrary.py
+++ TestLibrary.py
@@ -1,4 +1,5 @@
 import unittest
+from LibraryManagement import Book,Library
 
 class TestLibrary(unittest.TestCase):
      def setUp(self):
