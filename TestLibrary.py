79bdadcc3b7663792724dbb9dc3979210b217822
Henilgajera <henilgajera123@gmail.com>
2024-08-31 23:09:52 +0530

    Add testcase add_book_success

--- TestLibrary.py
+++ TestLibrary.py
@@ -2,8 +2,12 @@ import unittest
 from LibraryManagement import Book,Library
 
 class TestLibrary(unittest.TestCase):
-     def setUp(self):
+    def setUp(self):
             self.library = Library()
+            
+    def test_add_book_success(self):
+        
+        self.assertIn("1234567890", self.library.books)
 
 if __name__ == "__main__":
     unittest.main()
