e9132ec3c77cef2c36286de92c3dd8b37613bf5b
Henilgajera <henilgajera123@gmail.com>
2024-08-31 23:11:04 +0530

    Add Method add_book in Library

--- LibraryManagement.py
+++ LibraryManagement.py
@@ -9,3 +9,11 @@ class Book:
 class Library:
     def __init__(self):
         self.books = {}
+        
+    def add_book(self, isbn, title, author, publication_year):
+        """Add a new book to the library."""
+        if isbn in self.books:
+            raise ValueError("Book with ISBN {} already exists.".format(isbn))
+        book = Book(isbn, title, author, publication_year)
+        self.books[isbn] = book
+
--- TestLibrary.py
+++ TestLibrary.py
@@ -4,6 +4,9 @@ from LibraryManagement import Book,Library
 class TestLibrary(unittest.TestCase):
     def setUp(self):
             self.library = Library()
+            self.library.add_book("1234567890", "Rich Dad Poor Dad", "Robert Kiyosaki", 2000)
+            self.library.add_book("2345678901", "The Psychology of Money", "Morgan Housel", 20001)
+
             
     def test_add_book_success(self):
         
